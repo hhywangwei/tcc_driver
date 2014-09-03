731108c1e79ef9629de1ac72438b3b4c32febdec
WangWei <hhywangwei@gmail.com>
2014-09-04 00:31:35 +0800

    build login command

--- etone_driver/src/main/java/com/ewinsh/etone/server/command/builder/LoginBuilder.java
+++ etone_driver/src/main/java/com/ewinsh/etone/server/command/builder/LoginBuilder.java
@@ -3,11 +3,28 @@ package com.ewinsh.etone.server.command.builder;
 import com.alibaba.fastjson.JSONObject;
 import com.ewinsh.etone.driver.command.Commandable;
 import com.ewinsh.etone.driver.command.LoginCommand;
+import com.ewinsh.etone.server.Response;
 
 class LoginBuilder extends BaseCommandBuilder{
 	private static final String OPNUMBER_FIELD = "opNumber";
 	private static final String PASSWORD_FIELD = "password";
 	private static final String TYPE_FIELD = "TYPE_FILED";
+	
+	@Override
+	public boolean validate(JSONObject o, Response response){
+		boolean v = true;
+		if(isNotBlank(o, OPNUMBER_FIELD)){
+			v = false;
+			response.putErrorField(OPNUMBER_FIELD, "error.opnumber.notblank");
+		}
+		if(isNotBlank(o, PASSWORD_FIELD)){
+			v = false;
+			response.putErrorField(PASSWORD_FIELD, "error.password.notblank");
+		}
+		v = v & super.validate(o, response);
+		
+		return v;
+	}
 
 	@Override
 	public Commandable build(JSONObject o) {
